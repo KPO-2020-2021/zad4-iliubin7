21695c9a4ef045f069d343d47a40965cec386a12
Ilia Liubin <257798@student.pwr.edu.pl>
2021-05-21 23:59:31 +0300

    Dodanie konkretyzacji szablonu klasy Vector

---
+++ include/vector2D.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "vector.hpp"
+#include "matrix2D.hpp"
+#include "../src/vector2D.cpp"
+
+typedef Vector<double, 2> Vector2D;
+
+/*!
+* \brief Metoda rotacji wektora wokol srodka ukladu wspolrzednych
+* Argmunety:
+*  angle - kat obrotu (podany w stopniach)
+* Zwraca:
+*      rotated - obrocony wektor
+*/
+template < >
+inline Vector<double, 2> Vector<double, 2>::rotation(const double &angle) const;
+
---
+++ include/vector3D.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include "vector.hpp"
+
+typedef Vector<double, 3> Vector3D;
+
+#include "../src/vector3D.cpp"
